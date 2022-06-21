bc6bb94902c378320c8de4fd4958be98c8076b14
Júlio César <juliocsscl@gmail.com>
2022-06-21 00:27:09 -0300

    create firt block of page

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import Inicio from './components/Inicio/Inicio';
+
+function App() {
+  return (
+    <div className="App">
+      <Inicio />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Inicio/Inicio.jsx
@@ -0,0 +1,24 @@
+import react from 'react'
+import './inicio.css'
+
+export default function Inicio() {
+    return (
+        <section className='inicio'>
+            <div className='imagem-mac'>
+
+                <div>
+
+                </div>
+               
+
+                <div className="frontend">
+                    <h3>Júlio Norberto</h3>
+                    <h1>DEV FRONT-END</h1>
+
+                    <span> <a className='sobre' href="">Sobre</a> </span> <span className='espacamento-links'>ou</span> <span> <a className='contato' href="">Contato</a> </span>
+                </div>
+                
+            </div>
+        </section>
+    )
+}
