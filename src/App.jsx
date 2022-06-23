b527a5cc6358c2d34f89280ca544101b091c4a98
Júlio César <juliocsscl@gmail.com>
2022-06-22 23:11:44 -0300

    create mid part of site

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css';
 import Inicio from './components/Inicio/Inicio';
 import Caracteristicas from './components/Caracteristicas/Caracteristicas';
+import Mid from './components/Mid/Mid';
 
 function App() {
   return (
     <div className="App">
       <Inicio />
       <Caracteristicas />
+      <Mid />
     </div>
   );
 }
---
+++ src/components/Mid/Mid.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import './mid.css'
+import phone2 from '../../assets/phone-2.png'
+
+export default function Mid() {
+    return (
+        <section className="mid">
+
+            <div className="site-phone">
+                <h2>Foco na excelência da criação de cada site</h2>
+
+                <img width={350} src={phone2} alt="" />
+            </div>
+
+            
+
+            <div className="text-mid">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta aperiam, odio molestiae perspiciatis illo eaque ratione accusantium inventore doloribus explicabo tempore, aut consequuntur adipisci, incidunt unde praesentium numquam magnam placeat! Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque molestiae cupiditate odit iusto dolores mollitia culpa quo optio iste, deserunt ratione quibusdam vero. Unde obcaecati natus eligendi quae exercitationem. Enim.
+                </p> 
+            </div>
+
+        </section>
+    )
+}
